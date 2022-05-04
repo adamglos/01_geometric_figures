135d38fff21a1b5c153c01308cfeb05a7fc6b5d5
adamg <adamglos@gmail.com>
2022-05-04 20:04:29 +0200

    radius, diameter, area as properties

--- geometric_figures/geometric_figures.py
+++ geometric_figures/geometric_figures.py
@@ -3,11 +3,45 @@ from math import pi, sqrt
 
 class Circle:
     def __init__(self, radius):
-        self.radius = radius
-        self.diameter = self.radius * 2
-        self.area = pi * self.radius**2
+        self._radius = radius
+        self._diameter = self.radius * 2
+        self._area = pi * self.radius**2
 
     def __str__(self) -> str:
         return str(
             f"Koło (promień={self.radius}, średnica={self.diameter}, pole={round(self.area, 2)})"
         )
+
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value >= 0:
+            self._radius = value
+            self._diameter = self.radius * 2
+            self._area = pi * self.radius**2
+        else:
+            raise ValueError("Promień nie może być ujemny!")
+
+    @property
+    def diameter(self):
+        return self._diameter
+
+    @diameter.setter
+    def diameter(self, value):
+        self._diameter = value
+        self._radius = self.diameter / 2
+        self._area = pi * self.radius**2
+
+    @property
+    def area(self):
+        return self._area
+
+    @area.setter
+    def area(self, value):
+        if value > 0:
+            self._area = value
+            self._radius = sqrt(self.area / pi)
+            self._diameter = self._radius / 2
